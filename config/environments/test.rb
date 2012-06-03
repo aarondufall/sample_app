dedd615639b3f1cebfb10f27fc55924aac4a06d6
Aaron Dufall <aald212@gmail.com>
2012-06-03 20:08:28 +0800

    finish user sign

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,11 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+  
+  # Seed up Tests by lowering Bcrypt's cost function
+  # Speed up tests by lowering BCrypt's cost function. require 'bcrypt'
+  require 'bcrypt'
+  silence_warnings do
+      BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+    end
 end
